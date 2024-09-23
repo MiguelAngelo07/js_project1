c13cc0a51e4d02c42d7e4760f1d26e93469860c4
MiguelAngelo07 <miguelsouza161220@gmail.com>
2024-09-23 16:49:40 -0400

    alguns exercícios realizados

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
-let chute = prompt('Escolha um número entre 1 e 10');
+// alert('Boas vindas ao nosso site');
+// let numeroSecreto = 5;
+// let chute = prompt('Escolha um número entre 1 e 10');
+// let nome = "Lua";
+// let idade = 25;
+// let numeroDeVendas = 50;
+// let saldoDisponivel =  100;
+//let mensagemDeErro = "Erro preencha todos os campos";
+// alert(mensagemDeErro);
 
-if(numeroSecreto == chute) {
-    console.log('Isso ai! Você descobriu o número secreto (5)')
-}
+// if(numeroSecreto == chute) {
+//    console.log('Isso ai! Você descobriu o número secreto (5)')
+//};
+
+let idade = prompt('Qual a sua idade?');
+let nome = prompt('Qual o seu nome?');
+
+if (idade >= 18) {
+    alert('Você pode tirar sua habilitação')
+};
