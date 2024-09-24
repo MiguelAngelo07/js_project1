3722a6a6682e861b44e818dc71966a07d8582bc8
MiguelAngelo07 <miguelsouza161220@gmail.com>
2024-09-24 15:14:17 -0400

    incrementando o else

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-// alert('Boas vindas ao nosso site');
-// let numeroSecreto = 5;
-// let chute = prompt('Escolha um número entre 1 e 10');
+alert('Boas vindas ao nosso site');
+let numeroSecreto = 5;
+console.log(numeroSecreto)
+let chute = prompt('Escolha um número entre 1 e 10');
 // let nome = "Lua";
 // let idade = 25;
 // let numeroDeVendas = 50;
@@ -8,13 +9,15 @@
 //let mensagemDeErro = "Erro preencha todos os campos";
 // alert(mensagemDeErro);
 
-// if(numeroSecreto == chute) {
-//    console.log('Isso ai! Você descobriu o número secreto (5)')
+if (numeroSecreto == chute) {
+    alert('Isso ai! Você descobriu o número secreto (5)')
+} else {
+    alert('Você errou ;(')
+}
+
+//let idade = prompt('Qual a sua idade?');
+//let nome = prompt('Qual o seu nome?');
+
+//if (idade >= 18) {
+ //   alert('Você pode tirar sua habilitação')
 //};
-
-let idade = prompt('Qual a sua idade?');
-let nome = prompt('Qual o seu nome?');
-
-if (idade >= 18) {
-    alert('Você pode tirar sua habilitação')
-};
