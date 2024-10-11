fa316285500025a09f44101be782c1ee604411ca
MiguelAngelo07 <miguelsouza161220@gmail.com>
2024-10-10 23:05:22 -0400

    voltando

--- app.js
+++ app.js
@@ -2,22 +2,9 @@ alert('Boas vindas ao nosso site');
 let numeroSecreto = 5;
 console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 10');
-// let nome = "Lua";
-// let idade = 25;
-// let numeroDeVendas = 50;
-// let saldoDisponivel =  100;
-//let mensagemDeErro = "Erro preencha todos os campos";
-// alert(mensagemDeErro);
 
 if (numeroSecreto == chute) {
     alert('Isso ai! Você descobriu o número secreto (5)')
 } else {
     alert('Você errou ;(')
 }
-
-//let idade = prompt('Qual a sua idade?');
-//let nome = prompt('Qual o seu nome?');
-
-//if (idade >= 18) {
- //   alert('Você pode tirar sua habilitação')
-//};
